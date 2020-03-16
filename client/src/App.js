976c1766f3985ab32caa1d46b846b8d3a6f92d35
Tom Pocius <poci14@gmail.com>
2020-03-15 21:26:05 -0500

    added sass

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './pages/Home'
+import NavBar from './components/NavBar';
 
 
 function App() {
   return (
-    <Router>
-      <div>
-        <Switch>
-          <Route exact path="/">
-            <Home></Home>
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+    <div>
+      <NavBar></NavBar>
+      <Router>
+        <div>
+          <Switch>
+            <Route exact path="/">
+              <Home></Home>
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    </div>
   );
 }
 
---
+++ client/src/components/NavBar.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const NavBar = () => {
+
+
+  return (
+    <div className="primary">
+      NAVBAR
+      <button>this is a button</button>
+    </div>
+  )
+}
+
+export default NavBar;
