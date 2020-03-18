25ab5cb074421287aa6f6fe680c701451c337951
Tom Pocius <poci14@gmail.com>
2020-03-18 11:54:30 -0500

    fixed formatting on grid and added shadows

--- client/src/components/Angles.js
+++ client/src/components/Angles.js
@@ -4,9 +4,9 @@ import React from 'react';
 const Angles = () => {
   return (
     <>
-      <div className="item-style item-twa">TWA</div>
-      <div className="item-style item-atm">Angle To Mark</div>
-      <div className="item-style item-aamtnm">Angle after mark to next mark</div>
+      <div className="shadow-dark frow content-center item-twa">TWA</div>
+      <div className="shadow-dark frow content-center item-atm">Angle To Mark</div>
+      <div className="shadow-dark frow content-center item-aamtnm">Angle after mark to next mark</div>
     </>
   )
 }
--- client/src/components/Speeds.js
+++ client/src/components/Speeds.js
@@ -3,9 +3,9 @@ import React from 'react'
 const Speeds = () => {
   return (
     <>
-      <div className="item-style item-tws">TWS</div>
-      <div className="item-style item-aws">AWS</div>
-      <div className="item-style item-bs">Boat Speed</div>
+      <div className="item-tws shadow-dark frow content-center">TWS</div>
+      <div className="item-aws shadow-dark frow content-center">AWS</div>
+      <div className="item-bs shadow-dark frow content-center">Boat Speed</div>
     </>
   )
 }
