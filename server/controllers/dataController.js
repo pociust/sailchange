84d8a967786f1fff6b85e29ddaac1e079168b14c
Tom Pocius <poci14@gmail.com>
2020-03-19 15:31:02 -0500

    added find all and find by id in controller

--- server/controllers/dataController.js
+++ server/controllers/dataController.js
@@ -7,10 +7,10 @@ module.exports = {
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err))
   },
-  // findById: function (req, res) {
-  //   db.Sail_data.findById(req.params.id)
-  //     .then(dbModel => res.json(dbModel))
-  //     .catch(err => res.status(423).json(err));
-  // },
+  findById: function (req, res) {
+    db.Sail_data.findById(req.params.id)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(423).json(err));
+  },
 
 }
