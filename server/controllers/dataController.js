11fae55ba11c40cc14c674ba863ef6d777bc06c1
Tom Pocius <poci14@gmail.com>
2020-03-19 15:25:15 -0500

    have find by id when needed commentetd out

--- server/controllers/dataController.js
+++ server/controllers/dataController.js
@@ -1,10 +1,16 @@
-const db = require('../');
+const db = require('../models');
 
 
 module.exports = {
   findAll: function (req, res) {
-    db.data.find({})
+    db.Sail_data.find({})
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err))
-  }
+  },
+  // findById: function (req, res) {
+  //   db.Sail_data.findById(req.params.id)
+  //     .then(dbModel => res.json(dbModel))
+  //     .catch(err => res.status(423).json(err));
+  // },
+
 }
