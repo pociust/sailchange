9e08d1b7569c8e39e0603d853a560e73b3594c50
Tom Pocius <poci14@gmail.com>
2020-03-19 17:19:37 -0500

    style card for selecting boat

--- client/src/components/BoatSelect.js
+++ client/src/components/BoatSelect.js
@@ -4,7 +4,7 @@ import React from "react";
 const BoatSelect = (props) => {
   console.log('22222', props.boat)
   return (
-    <div>
+    <div className="frow justify-start p-5">
       <input className="shadow-light" type="checkbox" boat={props.boat}></input>
       <label className="shadow-dark">{props.boat}</label>
     </div>
