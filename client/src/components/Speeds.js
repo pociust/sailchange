4f2940951f76f576704c834750b3951510b7a235
Tom Pocius <poci14@gmail.com>
2020-03-23 21:51:10 -0500

    fixed some sass errors for mt

--- client/src/components/Speeds.js
+++ client/src/components/Speeds.js
@@ -3,9 +3,9 @@ import React from 'react'
 const Speeds = () => {
   return (
     <>
-      <div className="item-tws shadow-dark frow content-center">TWS</div>
-      <div className="item-aws shadow-dark frow content-center">AWS</div>
-      <div className="item-bs shadow-dark frow content-center">Boat Speed</div>
+        <div className="item-tws shadow-dark frow content-center mt-15">TWS</div>
+        <div className="item-aws shadow-dark frow content-center mt-15">AWS</div>
+        <div className="item-bs shadow-dark frow content-center mt-15">Boat Speed</div>
     </>
   )
 }
