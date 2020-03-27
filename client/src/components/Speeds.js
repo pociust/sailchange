ee2f4decfe27dd6e438a22c2a02309bc9418f44e
Tom Pocius <poci14@gmail.com>
2020-03-27 11:59:19 -0500

    passed hardcoded data to cards on sailchange screen

--- client/src/components/Speeds.js
+++ client/src/components/Speeds.js
@@ -1,11 +1,16 @@
 import React from 'react'
 
-const Speeds = () => {
+const Speeds = (props) => {
   return (
     <>
-        <div className="item-tws shadow-dark frow content-center mt-15">TWS</div>
-        <div className="item-aws shadow-dark frow content-center mt-15">AWS</div>
-        <div className="item-bs shadow-dark frow content-center mt-15">Boat Speed</div>
+      <div className="item-tws shadow-dark frow content-center mt-15">
+        <div>
+          TWS:
+          </div>
+        <div>{props.wind}</div>
+      </div>
+      <div className="item-aws shadow-dark frow content-center mt-15">AWS</div>
+      <div className="item-bs shadow-dark frow content-center mt-15">Boat Speed</div>
     </>
   )
 }
