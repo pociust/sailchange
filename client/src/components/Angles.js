e060b5729f9f7d407fb0f9302c9163d38c396987
Tom Pocius <poci14@gmail.com>
2020-03-27 11:59:05 -0500

    passed hardcoded data to cards on sailchange screen

--- client/src/components/Angles.js
+++ client/src/components/Angles.js
@@ -1,10 +1,19 @@
 import React from 'react';
 
 
-const Angles = () => {
+const Angles = (props) => {
   return (
     <>
-      <div className="shadow-dark frow content-center item-twa">TWA</div>
+      <div className="shadow-dark frow content-center item-twa">
+        <div>
+          TWA:
+        </div>
+        <div>
+          {props.windDirection}deg
+        </div>
+        <div>AWA:</div>
+        <div></div>
+      </div>
       <div className="shadow-dark frow content-center item-atm">Angle To Mark</div>
       <div className="shadow-dark frow content-center item-aamtnm">Angle after mark to next mark</div>
     </>
